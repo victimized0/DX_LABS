4ca6b088d1f378cd83af632e6b8953e38940bc30
victimized0 <hayalisalimov@protonmail.com>
2020-03-22 18:39:55 +0300

    clean up

--- DX_Engine/Include/Interfaces/IGameObject.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INTERFACE_GAME_OBJECT_H_
-#define _INTERFACE_GAME_OBJECT_H_
-
-class IGameObject {
-	virtual void Initialise() = 0;
-	virtual void Update(float dt) = 0;
-};
-
-#endif //_INTERFACE_GAME_OBJECT_H_
--- DX_Engine/post-build.bat
+++
@@ -1,4 +0,0 @@
-set ProjectDir=%1
-
-rmdir "%ProjectDir%\Include\"
-xcopy "%ProjectDir%\Code\*.h" "%ProjectDir%\Include\" /exclude:exclude.txt /sy
